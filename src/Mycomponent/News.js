08c00d4917ae5d1294e5cf9c6e94a8692ee46cb1
Irtaza Ahmed <irtazaahmedk@gmail.com>
2024-09-05 22:35:57 +0500

    react routing

--- src/Mycomponent/News.js
+++ src/Mycomponent/News.js
@@ -39,7 +39,7 @@ export default class News extends Component {
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=57b510932495419f9c96cb27e56e45b8&pageSize=${this.props.pageSize}`;
     let data = await fetch(url) 
     let parsedData = await data.json()
-    console.log(parsedData)
+    // console.log(parsedData)
     this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults, loading:false})
 
    
@@ -56,7 +56,7 @@ export default class News extends Component {
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=57b510932495419f9c96cb27e56e45b8&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url) 
     let parsedData = await data.json()
-    console.log(parsedData)
+    // console.log(parsedData)
    
 
     this.setState({
@@ -72,7 +72,7 @@ export default class News extends Component {
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=57b510932495419f9c96cb27e56e45b8&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url) 
     let parsedData = await data.json()
-    console.log(parsedData)
+    // console.log(parsedData)
    
 
     this.setState({
@@ -89,7 +89,7 @@ export default class News extends Component {
       <div className='container my-3'>
        
 
-        <h1 className='text-center mt-3 mb-3'>NewsMonkey - Top Headlines</h1>
+        <h1 className='text-center mt-3 mb-3' style={{margin: "35px 0px"}}>NewsMonkey - Top Headlines</h1>
         {this.state.loading && <Spinner />}
         <div className='d-flex justify-content-center mt-3 flex-wrap'>
           {!this.state.loading && this.state.articles.map((element)=> {
