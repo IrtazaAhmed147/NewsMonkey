25d4c87ccf706b6d16357cee43f18598bd2a2cb2
Irtaza Ahmed <irtazaahmedk@gmail.com>
2024-09-06 20:16:35 +0500

    infinite scrolling

--- src/Mycomponent/News.js
+++ src/Mycomponent/News.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import NewsItems from "./NewsItems";
 import Spinner from "./Spinner";
 import PropTypes from 'prop-types';
+import InfiniteScroll from "react-infinite-scroll-component";
+
 
 
 
@@ -20,7 +22,7 @@ export default class News extends Component {
   //   category: propTypes.string,
   // }
 
-  capatalize = (string)=> {
+  capatalize = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1)
   }
 
@@ -31,6 +33,7 @@ export default class News extends Component {
       articles: [],
       loading: false,
       page: 1,
+      totalResults: 0,
 
     }
     document.title = `NewMonkey - ${this.capatalize(this.props.category)}`
@@ -53,18 +56,35 @@ export default class News extends Component {
 
   }
 
-  handleNextBtn = async () => {
+  // handleNextBtn = async () => {
 
+  //   this.setState({ page: this.state.page + 1 })
+  //   this.updateNews()
+
+  // }
+  // handlePrevBtn = async () => {
+
+  //   this.setState({ page: this.state.page - 1 })
+  //   this.updateNews()
+
+  // }
+
+  fetchMoreData = async () => {
     this.setState({ page: this.state.page + 1 })
-    this.updateNews()
 
-  }
-  handlePrevBtn = async () => {
+    // this.setState({ loading: true })
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=57b510932495419f9c96cb27e56e45b8&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url)
+    let parsedData = await data.json()
 
-    this.setState({ page: this.state.page - 1 })
-    this.updateNews()
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults,
+      // loading: false
+    })
 
-  }
+  };
+
 
 
   render() {
@@ -73,28 +93,45 @@ export default class News extends Component {
 
         {/* <div className='d-flex justify-content-between align-items-center container flex-wrap'> */}
 
-          <h2 className='text-center' style={{ margin: "35px 0px" }}>NewsMonkey - Top {this.capatalize(this.props.category)} Headlines</h2>
-          {/* <h3 style={{ margin: "35px 0px" }}>Category-{}</h3> */}
+        <h2 className='text-center' style={{ margin: "35px 0px" }}>NewsMonkey - Top {this.capatalize(this.props.category)} Headlines</h2>
+
+        {/* <h3 style={{ margin: "35px 0px" }}>Category-{}</h3> */}
         {/* </div> */}
+        {/* {this.state.loading && <Spinner />} */}
 
-        {this.state.loading && <Spinner />}
 
-        <div className='d-flex justify-content-center mt-3 flex-wrap'>
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
 
-          {!this.state.loading && this.state.articles.map((element) => {
-            return <div key={element.url}>
+          <div className="container">
 
-              <NewsItems title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} publishedAt={element.publishedAt} source={element.source.name} />
 
+            <div className='d-flex justify-content-center mt-3 flex-wrap'>
+
+              {this.state.articles.map((element) => {
+                return <div key={element.url}>
+
+                  <NewsItems title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} publishedAt={element.publishedAt} source={element.source.name} />
+
+                </div>
+
+              })}
             </div>
+          </div>
 
-          })}
-        </div>
+        </InfiniteScroll>
 
-        <div className='container d-flex justify-content-between'>
+
+
+
+        {/* <div className='container d-flex justify-content-between'>
           <button disabled={this.state.page <= 1} type='button' className='btn btn-dark' onClick={this.handlePrevBtn}>&larr; Previous</button>
           <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type='button' className='btn btn-dark' onClick={this.handleNextBtn}>Next &rarr;</button>
-        </div>
+        </div> */}
 
 
       </div>
