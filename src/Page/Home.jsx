e704ed70c2435d9706a03d1f6d2df0153868aaec
Irtaza Ahmed <irtazaahmedk@gmail.com>
2025-01-14 00:18:07 +0500

    making slider

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,14 +1,17 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import HomeCards from '../Mycomponent/HomeCards'
 import { getLatestNews } from '../Api/Apis'
 import { useQuery } from '@tanstack/react-query'
 import Loader from '../Mycomponent/Loader'
 import CardLoader from '../Mycomponent/CardLoader'
 import { AppContext } from '../Context/Data'
+import { FaGreaterThan, FaLessThan } from 'react-icons/fa6'
 
 const Home = () => {
 
     const { timeSplit } = useContext(AppContext)
+    const [imageIndex, setImageIndex] = useState(0)
+    const [translate, setTranslate] = useState(0)
 
     const { data, isFetching } = useQuery({
         queryKey: ['latestNews'],
@@ -19,6 +22,17 @@ const Home = () => {
     })
     console.log(data)
     const firstNews = data?.articles[0]
+
+    const handleNextImage = ()=> {
+        setImageIndex(prev=> prev + 1)
+        setTranslate(imageIndex * 300)
+        console.log(translate)
+    }
+    const handlePrevImage = ()=> {
+        setImageIndex(prev=> prev - 1)
+        setTranslate(imageIndex * 300)
+        console.log(translate)
+    }
 
     return (
         <div className='py-6'>
@@ -50,6 +64,18 @@ const Home = () => {
                 {!isFetching && data?.articles?.map((news, i) => {
                     return <HomeCards key={i} {...news} />
                 })}
+            </div>
+            {/* slider */}
+            <div className='flex overflow-hidden gap-1 w-11/12 mx-auto relative mt-5'>
+                <div style={{transform: `translateX(-${translate}px)`,  transition: 'transform 0.3s ease-in-out'}} className='flex gap-1'>
+
+                {data?.articles?.map((news, i)=> {
+                    return  <img key={i} style={{width: '300px', height: '230px'}} src={news.urlToImage} alt="" />
+                    
+                })}
+                </div>
+                <button onClick={()=> handlePrevImage()} className='absolute w-12 top-16 left-4 rounded-full h-16 hover:bg-neutral-100 flex justify-center items-center'><FaLessThan /></button>
+                <button onClick={()=> handleNextImage()} className='absolute top-16 w-12 rounded-full right-4 h-16 hover:bg-neutral-100 flex justify-center items-center'><FaGreaterThan /></button>
             </div>
         </div>
     )
