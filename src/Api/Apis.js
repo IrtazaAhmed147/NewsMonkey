94f877c55b64a5c0b31620fc48c23eac0ad53146
Irtaza Ahmed <irtazaahmedk@gmail.com>
2025-01-30 21:00:11 +0500

    directly applying api to check error

--- src/Api/Apis.js
+++ src/Api/Apis.js
@@ -2,9 +2,10 @@ const apiKey = process.env.REACT_APP_NEWS_API
 
 export const getCategoryNews = async (category, pageNo) => {
     try {
-        const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${apiKey}&page=${pageNo}&pageSize=12`)
+        const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=57b510932495419f9c96cb27e56e45b8&page=${pageNo}&pageSize=12`)
         const res = await response.json()
 
+        console.log(res);
         if (res.status === 'ok') {
             return res
         }
@@ -15,9 +16,10 @@ export const getCategoryNews = async (category, pageNo) => {
 }
 export const getLatestNews = async () => {
     try {
-        const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`)
+        const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=57b510932495419f9c96cb27e56e45b8`)
         const res = await response.json()
-
+        console.log(res);
+        
         if (res.status === 'ok') {
             return res
         }
